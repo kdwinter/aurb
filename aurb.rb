a00c3b84415d551d8f80a5c9640233696d75fae4
Gigamo <gigamo@gmail.com>
2009-02-25 01:14:57 +0100

    Add license disclaimer and fix methods require

--- aurb.rb
+++ aurb.rb
@@ -5,9 +5,15 @@ AurB is a Ruby AUR utility, heavily inspired by `arson' and `yaourt'.
 Author: Gigamo <gigamo@gmail.com>
 License: WTFPL <http://sam.zoy.org/wtfpl/>
 
+  This program is free software. It comes without any warranty, to
+  the extent permitted by applicable law. You can redistribute it
+  and/or modify it under the terms of the Do What The Fuck You Want
+  To Public License, Version 2, as published by Sam Hocevar. See
+  http://sam.zoy.org/wtfpl/COPYING for more details.
+
 =end
 
-['optparse', 'pathname', 'lib/methods', 'rubygems', 'facets/ansicode', 'logger'].each do |lib|
+['optparse', 'pathname', 'rubygems', 'facets/ansicode', 'logger', File.dirname(__FILE__) + '/lib/methods'].each do |lib|
   require lib
 end
 
