ea152535ff3e7fa3ec619aab3c6e8c1a28da3264
Gigamo <gigamo@gmail.com>
2009-02-25 02:00:24 +0100

    Added comments to library files too

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,4 +1,17 @@
 #!/usr/bin/env ruby
+=begin
+
+AurB is a Ruby AUR utility, heavily inspired by `arson' and `yaourt'.
+Author: Gigamo <gigamo@gmail.com>
+License: WTFPL <http://sam.zoy.org/wtfpl/>
+
+  This program is free software. It comes without any warranty, to
+  the extent permitted by applicable law. You can redistribute it
+  and/or modify it under the terms of the Do What The Fuck You Want
+  To Public License, Version 2, as published by Sam Hocevar. See
+  http://sam.zoy.org/wtfpl/COPYING for more details.
+
+=end
 
 ['rubygems', 'zlib', 'facets/version', 'facets/minitar', 'facets/ansicode', 'json', 'cgi', 'open-uri', 'fileutils'].each do |lib|
   require lib
