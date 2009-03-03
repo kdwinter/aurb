1de38aa8373bf4a2eee6946c1f9ab323c176197a
Gigamo <gigamo@gmail.com>
2009-03-03 19:35:43 +0100

    Revert "Added install switch"
    This reverts commit 83a5f86a63bbdafd7927930d5a10790f45db0d83.

--- lib/aurb/methods.rb
+++ lib/aurb/methods.rb
@@ -161,11 +161,6 @@ module AurB
               FileUtils.chdir("#{$options[:download_dir]}/#{pkg}")
               puts "Building #{pkg} with makepkg.."
               exec 'makepkg'
-              STDOUT.puts "Do you want to install #{pkg}?\n"
-              answer = STDIN.gets
-              if answer.downcase == 'y'
-                exec "sudo pacman -U #{pkg}.pkg.tar.gz"
-              end
             end
           end
         end
@@ -194,11 +189,6 @@ module AurB
           FileUtils.chdir("#{$options[:download_dir]}/#{pkg}")
           puts "Building #{pkg} with makepkg.."
           exec 'makepkg'
-          STDOUT.puts "Do you want to install #{pkg}?\n"
-          answer = STDIN.gets
-          if answer.downcase == 'y'
-            exec "sudo pacman -U #{pkg}.pkg.tar.gz"
-          end
         end
       end
     else
