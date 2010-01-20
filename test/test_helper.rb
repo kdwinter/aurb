c16aee5dcbc3d6fc2fadc19dba561000a2a8dd01
Gigamo <gigamo@gmail.com>
2010-01-21 00:46:42 +0100

    Fixes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/aurb')
 require 'shoulda'
 
 class Test::Unit::TestCase
+  include Aurb
+
   def Session(&block)
     klass = Class.new do
       include Aurb::Aur
