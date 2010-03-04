0fcb0f1cdfd3e9d6205d3a617f2f56597e982818
Gigamo <gigamo@gmail.com>
2010-03-04 19:23:50 +0100

    Pass in parameters to Aur#search and Aur#download prefixed with *.

--- test/unit/test_aur.rb
+++ test/unit/test_aur.rb
@@ -26,7 +26,7 @@ class AurTest < Test::Unit::TestCase
       end
 
       should 'return an array' do
-        assert Aurb.aur.upgrade(@list).is_a?(Array)
+        assert_operator Array, :===, Aurb.aur.upgrade(@list)
       end
 
       should 'contain only upgradable packages' do
@@ -37,56 +37,55 @@ class AurTest < Test::Unit::TestCase
 
     context 'download' do
       setup do
-        @package_working = ['aurb']
-        @package_faulty  = ['foobarbaz']
+        @package_working = 'aurb'
+        @package_faulty  = 'foobarbaz'
+        @package_multi   = 'aurb', 'yaourt'
       end
 
       should 'return an array' do
-        assert Aurb.aur.download(@package_working).is_a?(Array)
-        assert Aurb.aur.download(@package_faulty).is_a?(Array)
+        assert_operator Array, :===, Aurb.aur.download(*@package_working)
+        assert_operator Array, :===, Aurb.aur.download(*@package_faulty)
       end
 
       should 'return download links' do
-        assert_equal [Aurb.aur_download_path(@package_working.join)], Aurb.aur.download(@package_working)
+        assert_equal [Aurb.aur_download_path(*@package_working)], Aurb.aur.download(*@package_working)
       end
 
       should 'filter out non-existant packages' do
-        assert Aurb.aur.download(@package_faulty).blank?
-        assert_equal [], Aurb.aur.download(@package_faulty)
+        assert Aurb.aur.download(*@package_faulty).blank?
+        assert_equal [], Aurb.aur.download(*@package_faulty)
       end
     end
 
     context 'search' do
       setup do
-        @package_working = ['aurb']
-        @package_faulty  = ['foobarbaz']
+        @package_working = 'aurb'
+        @package_faulty  = 'foobarbaz'
+        @package_multi   = 'aurb', 'yaourt'
       end
 
       should 'return an array of results' do
-        assert Aurb.aur.search(@package_working).is_a?(Array)
+        assert_operator Array, :===, Aurb.aur.search(*@package_working)
+        assert_operator Array, :===, Aurb.aur.search(*@package_faulty)
       end
 
       should 'filter out non-existant packages' do
-        assert Aurb.aur.search(@package_faulty).blank?
-        assert_equal [], Aurb.aur.search(@package_faulty)
+        assert Aurb.aur.search(*@package_faulty).blank?
+        assert_equal [], Aurb.aur.search(*@package_faulty)
       end
 
       context 'result' do
         setup do
-          @result = Aurb.aur.search(@package_working).first
+          @result = Aurb.aur.search(*@package_working).first
         end
 
         should 'return an array containing hashes' do
-          assert @result.is_a?(Hash)
+          assert_operator Hash, :===, @result
         end
 
         context 'keys' do
-          setup do
-            @key = @result.keys.first
-          end
-
           should 'be symbolized' do
-            assert @key.is_a?(Symbol)
+            assert_operator Symbol, :===, @result.keys.first
           end
         end
       end
