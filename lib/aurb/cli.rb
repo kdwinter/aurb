4fd2f92f1543a83197e90295fbe222af59060119
Gigamo <gigamo@gmail.com>
2010-03-06 13:00:10 +0100

    Expand load path in cli

--- lib/aurb/cli.rb
+++ lib/aurb/cli.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-$LOAD_PATH.unshift File.dirname(__FILE__) + '/..'
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/..')
 require 'thor'
 require 'aurb'
 
