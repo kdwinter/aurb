01c33264b8a07b076d840e934b3e7847bca72aa2
Gigamo <gigamo@gmail.com>
2010-03-07 15:57:07 +0100

    Fix upgrade comment

--- lib/aurb/aur.rb
+++ lib/aurb/aur.rb
@@ -68,7 +68,7 @@ module Aurb
     # the +download+ method.
     #
     #   # With Aurb on the AUR as version 1.1.2-1
-    #   upgrade('aurb 0.0.0-0', 'aurb 0.9.9-9') # => [:aurb]
+    #   upgrade('aurb 0.0.0-0', 'aurb 9.9.9-9') # => [:aurb]
     def upgrade(*list)
       upgradables = []
       list.inject([]) do |ary, line|
