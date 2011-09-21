d904fccd0db06e8f655ff5902a74612d2814b1a0
Gigamo <gigamo@gmail.com>
2011-09-21 21:20:49 +0200

    Hm

--- lib/aurb/main.rb
+++ lib/aurb/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-require 'rubygems' if RUBY_DESCRIPTION =~ /rubinius/i
+require 'rubygems' if RUBY_DESCRIPTION =~ /(rubinius|jruby)/i
 require 'thor'
 require File.expand_path('../../aurb', __FILE__)
 
